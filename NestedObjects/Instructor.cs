5958f8395e9e57bef6eef9ea2f40ae9bd4aadb98
NessRK13 <ness6514@student.cptc.edu>
2019-02-19 18:57:54 -0800

    Added documentation to the instructor class

--- NestedObjects/Instructor.cs
+++ NestedObjects/Instructor.cs
@@ -8,10 +8,20 @@ namespace NestedObjects
 {
     class Instructor
     {
+        /// <summary>
+        /// The instructors first and last name
+        /// ex. Daniel Ness
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The instructors work email
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// The office phone number for the instructor
+        /// </summary>
         public string PhoneNumber { get; set; }
 
     }
